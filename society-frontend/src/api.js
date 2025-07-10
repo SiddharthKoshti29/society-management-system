22ea8120cf19ce9d98f1697454ee4c4c1c37d55a
SiddharthKoshti29 <siddharthkoshti7470@gmail.com>
2025-07-10 12:12:53 +0530

    Update api.js

--- society-frontend/src/api.js
+++ society-frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://society-backend-xs4k.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
